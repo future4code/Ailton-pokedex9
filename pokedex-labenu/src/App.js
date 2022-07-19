b93ec8c174bb71068711939ee9bb7f6b7bda455c
unknown <rafael.tj18@hotmail.com>
2022-07-19 14:47:48 -0300

    Header

--- pokedex-labenu/src/App.js
+++ pokedex-labenu/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Headers from './components/headers/Headers';
+import React from 'react';
+import GlobalStyle from './globalStyles';
 
 function App() {
   return (
     <div>
+      < GlobalStyle />
+      <Headers />
       Pokedéx
     </div>
   );
